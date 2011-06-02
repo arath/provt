3786864f26f78ea59d3017646a6149814cd709e4
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-06-02 11:22:08 -0600

    Updated

--- build/configuration/db.rb
+++ build/configuration/db.rb
@@ -5,7 +5,6 @@ task :configure_db do
     {
       :initial_catalog => delayed{configatron.project},
       :server_name => ENV['HOSTNAME'],
-      :osql_connection_string => "-E",
       :web_account_sql => "#{configatron.db.web_user_account}, N'#{configatron.db.web_user_account}'",
       :app_connection => delayed{"data source=#{configatron.db.server_name};Integrated Security=SSPI;Initial Catalog=#{configatron.db.initial_catalog}"}
     }
