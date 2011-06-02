22353d9535df90049dd5cac9501d39c9be2fa74c
Your Name <you@email.com>
2011-06-02 13:23:30 -0600

    Pushing new changes

--- source/provident/Calculator.cs
+++ source/provident/Calculator.cs
@@ -1,9 +1,14 @@
-﻿namespace provident
+﻿using System;
+
+namespace provident
 {
   public class Calculator
   {
     public int add(int first_number, int second_number)
     {
+      if (first_number < 0 || second_number < 0)
+        throw new ArgumentException();
+
       return first_number + second_number;
     }
   }
